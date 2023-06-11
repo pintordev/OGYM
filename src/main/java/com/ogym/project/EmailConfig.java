f14797bcc2cf92ed989da0004643f698fcf024f0
ohgil <ohgilhwan1957@gmail.com>
2023-06-12 00:57:49 +0900

    ㅇ

---
+++ src/main/java/com/ogym/project/EmailConfig.java
@@ -0,0 +1,69 @@
+package com.ogym.project;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.io.IOException;
+import java.util.Properties;
+
+@Configuration
+public class EmailConfig {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public EmailConfig() throws IOException {
+        logger.info("EmailConfig.java constructor called");
+    }
+
+    @Value("${spring.mail.transport.protocol}")
+    private String protocol;
+
+    @Value("${spring.mail.properties.mail.smtp.auth}")
+    private boolean auth;
+
+    @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
+    private boolean starttls;
+
+    @Value("${spring.mail.debug}")
+    private boolean debug;
+
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.port}")
+    private int port;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Value("${spring.mail.default.encoding}")
+    private String encoding;
+
+
+    @Bean
+    public JavaMailSender javaMailSender(){
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        Properties properties = new Properties();
+        properties.put("mail.transport.protocol", protocol);
+        properties.put("mail.smtp.auth", auth);
+        properties.put("mail.smtp.starttls.enable", starttls);
+        properties.put("mail.smtp.debug", debug);
+
+        mailSender.setHost(host);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+        mailSender.setPort(port);
+        mailSender.setJavaMailProperties(properties);
+        mailSender.setDefaultEncoding(encoding);
+
+        return mailSender;
+    }
+}
--- src/main/java/com/ogym/project/user/UserController.java
+++ src/main/java/com/ogym/project/user/UserController.java
@@ -1,9 +1,10 @@
 package com.ogym.project.user;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/user")
 @RequiredArgsConstructor
@@ -11,11 +12,48 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     private final UserService userService;
+    private final UserEmailService userEmailService;
+
 
     @GetMapping("/signup")
-    public String signup() {
+    public String signup(UserCreateForm userCreateForm) {
         return "signup";
     }
+
+    public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "signup_form";
+        }
+
+        // 비밀번호와 비밀번호 확인에 입력한 문자열이 서로 다르면 다시 입력 하도록
+        if (!userCreateForm.getPassword().equals(userCreateForm.getPasswordCheck())) {
+            bindingResult.rejectValue("password2", "passwordInCorrect",
+                    "입력한 비밀번호가 일치하지 않습니다.");
+            return "signup_form";
+        }
+
+        if (!this.userService.confirmCertificationCode(userCreateForm.getCode(), userCreateForm.getGenCode())) {
+            bindingResult.rejectValue("inputCode", "codeInCorrect",
+                    "입력한 인증번호가 일치하지 않습니다.");
+            return "signup_form";
+        }
+
+        userService.create(userCreateForm.getLoginId(), userCreateForm.getPassword(), userCreateForm.getEmail(), userCreateForm.getName(), userCreateForm.getBirthYear(),
+                userCreateForm.getBirthMonth(),userCreateForm.getBirthDay(),userCreateForm.getPhone());
+
+        return "redirect:/user/login";
+    }
+
+    @PostMapping("/signup/emailConfirm")
+    @ResponseBody
+    public String emailConfirm(@RequestParam("email") String email) {
+        String genCode = this.userService.genConfirmCode(8);
+        System.out.println(genCode);
+        this.userEmailService.mailSend(email, "이메일 인증", genCode);
+        return this.userService.getEmailConfirmCode(genCode);
+    }
+
+
 
     @GetMapping("/login")
     public String login() {
