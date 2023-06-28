be5889311e8ba6c2c391fbc591011ab2b9ddcab4
pintordev <pintor.dev@gmail.com>
2023-06-28 10:44:43 +0900

    white-space: nowrap

--- src/main/java/com/ogym/project/file/FileController.java
+++ src/main/java/com/ogym/project/file/FileController.java
@@ -1,16 +1,32 @@
 package com.ogym.project.file;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.File;
+
+import static org.thymeleaf.util.StringUtils.concat;
 
 @RequestMapping("/file")
 @RequiredArgsConstructor
 @Controller
 public class FileController {
 
+    private final String presentPath = System.getProperty("user.dir")
+            + File.separator + "upload";
 
+    @GetMapping("/userProfileUpload")
     public String userProfileUpload() {
-        return "";
+
+        System.out.println("현재 작업 경로: " + presentPath);
+
+//        File file = new File();
+//        file.mkdir();
+
+        return "file";
     }
 }
