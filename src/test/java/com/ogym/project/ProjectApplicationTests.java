b11c2e82db4aea9f86f6ac0096cc3da5a6834197
ohgil <ohgilhwan1957@gmail.com>
2023-07-13 09:07:21 +0900

    html & css update

--- src/test/java/com/ogym/project/ProjectApplicationTests.java
+++ src/test/java/com/ogym/project/ProjectApplicationTests.java
@@ -2,6 +2,8 @@ package com.ogym.project;
 
 import com.ogym.project.trainer.trainer.Trainer;
 import com.ogym.project.trainer.trainer.TrainerRepository;
+import com.ogym.project.user.user.SiteUser;
+import com.ogym.project.user.user.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,11 +16,14 @@ class ProjectApplicationTests {
 	@Autowired
 	private TrainerRepository trainerRepository;
 
+	@Autowired
+	private UserRepository userRepository;
+
 	@Test
 	void testJpa() {
-		Optional<Trainer> optionalTrainer = this.trainerRepository.findById(12L);
-		if (optionalTrainer.isPresent()) {
-			this.trainerRepository.delete(optionalTrainer.get());
-		}
+//		Optional<SiteUser> optionalSiteUser = this.userRepository.findById(82L);
+//		if (optionalSiteUser.isPresent()) {
+//			this.userRepository.delete(optionalSiteUser.get());
+//		}
 	}
 }
